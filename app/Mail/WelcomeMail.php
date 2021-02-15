90823202c2f7acaaec32236de47b3d5190a4c86b
Jhonny Ribeiro <o.jhonny.ribeiro@gmail.com>
2021-02-15 10:19:42 -0300

    email fix url

--- app/Mail/WelcomeMail.php
+++ app/Mail/WelcomeMail.php
@@ -36,7 +36,7 @@ class WelcomeMail extends Mailable
         return $this->view('emails.welcome')
             ->subject('Bem-vindo ao '.config('app.name'))
             ->with([
-                'verifyEmailLink' => config('app.url').'/verify-mail?token='.$this->user->confirmation_token,
+                'verifyEmailLink' => config('app.url').'/verificar-email?token='.$this->user->confirmation_token,
             ]);
     }
 }
